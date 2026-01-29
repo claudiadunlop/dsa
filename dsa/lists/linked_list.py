779ecfc9ffa972ab09a3a0f896cb0d2e839e5356
Claudia Dunlop <claudia.dunlop@umontana.edu>
2026-01-29 14:03:59 -0700

    class

--- dsa/lists/linked_list.py
+++ dsa/lists/linked_list.py
@@ -26,26 +26,55 @@ class LinkedList(Sequence[T]):
 
     def __init__(self):
         """Create an empty linked list."""
-        raise NotImplementedError
+        self._head = None
+        self._n = 0
+        self._tail = None
+        #raise NotImplementedError
 
     def append(self, value: T) -> None:
-
-        raise NotImplementedError
+        if self._n == 0:
+            self._head = self._Node(value, next = None)
+        else:
+            current_node = self._head
+            while current_node._next is not None:
+                current_node = current_node._next
+            current_node._next = self._Node(value)
+        self._n += 1
+        #raise NotImplementedError
 
     def __getitem__(self, index: int) -> T:
-        raise NotImplementedError
+        current_node = self._head
+        for i in range(index):
+            current_node = current_node._next
+        return current_node
+        #raise NotImplementedError
 
     def __setitem__(self, index: int, value: T) -> None:
-        raise NotImplementedError
+        current_node = self._head
+        for i in range(index):
+            current_node = current_node._next
+        current_node = value
+        return current_node
+        #raise NotImplementedError
 
     def __delitem__(self, index: int) -> None:
-        raise NotImplementedError
+        current_node = self._head
+        for i in range(index):
+            current_node = current_node._next
+        current_node = None
+        self._n -= 1
+        #raise NotImplementedError
 
     def insert(self, index: int, value: T) -> None:
+        current_node = self._head
+    
         raise NotImplementedError
 
     def __len__(self) -> int:
-        raise NotImplementedError
+        return self._n
+        #raise NotImplementedError
 
     def __iter__(self) -> Iterator[T]:
-        raise NotImplementedError
+        for i in range(self._n):
+            yield self[i]
+        #raise NotImplementedError
